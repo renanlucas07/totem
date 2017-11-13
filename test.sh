bdecd1b0a65ee08d0dbee7484003ae5659d6a5f1
renanlucas07 <renan.lucas07@gmail.com>
2017-11-13 14:14:06 -0300

    Fixed centralizing issues

--- test.sh
+++ test.sh
@@ -3,13 +3,13 @@
 rm file.txt
 
 
-echo ${#2}
-echo $((100-${#2}))
+#echo ${#2}
+#echo $((100-${#2}))
     echo L >> file.txt
     echo D11 >> file.txt
     echo H12 >> file.txt
-    echo 1911004003000$((157-${#2}*9/2))$2 >> file.txt
-    echo 1911004001200$((157-${#3}*9/2))$3 >> file.txt
+    echo 1911004003000$((157-${#2}*13/3))$2 >> file.txt
+    echo 1911004001200$((157-${#3}*13/3))$3 >> file.txt
     echo D22 >> file.txt
     echo 1e1104000600070$1 >> file.txt
     echo E >> file.txt
